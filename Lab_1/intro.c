c9ce37213181dc6a5d77a903799e8c2eed5754d2
abhay1412007 <abhayjha9006@gmail.com>
2025-10-22 20:42:33 +0530

    Create intro.c

---
+++ Lab_1/intro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  // printing personal details
+  printf("Name: Abhay Kumar Jha\n");
+  printf("Roll Number: MEB25061\n");
+  printf("Department: Mechanical Engineering\n");
+  printf("University: Tezpur University\n");
+  
+  // ending the program
+  return 0;
+}
